443672367d7b250a474b4c3e39e98d6698fa316f
Gokul Nair <gokulgnair94@gmail.com>
2017-06-10 17:49:40 +0530

    Test

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var jsonQuery = require('json-query');
 
 
 restService.post('/inputmsg', function(req, res) {
+	console.log( "Req : " + JSON.stringify(req));
 
     var intentName = req.body.result.metadata.intentName;
     console.log( "intentName : " + intentName );
