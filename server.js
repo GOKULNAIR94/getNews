6b876bbad1bd76c398d349b62dc322524414e464
Gokul Nair <gokulgnair94@gmail.com>
2017-09-23 14:21:55 +0530

    News

--- server.js
+++ server.js
@@ -72,101 +72,36 @@ restService.post('/inputmsg', function(req, res) {
                                 speech = speech + "" + os.EOL + "" + data.title + "! ";
                                 speech = speech + "\n More @ : " + shortUrl + "!" + os.EOL;
                                 returnJson = {
+                                    speech: "Hi",
+                                    displayText: "Hi",
+                                    data: {
+                                        google: {
                                             'expectUserResponse': true,
-                                            "conversationToken": "{\"state\":null,\"data\":{}}",
-                                            "expectedInputs": [
-        {
-            "inputPrompt": {
-                "richInitialPrompt": {
-                    "items": [
-                        {
-                            "simpleResponse": {
-                                "textToSpeech": "Alright! Here are a few things you can learn. Which sounds interesting?",
-                                "displayText": 'Hi! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!'
-                            }
-                        }
-                    ],
-                    "suggestions": [
-                        {
-                            "title": "Basic Card"
-                        },
-                        {
-                            "title": "List"
-                        },
-                        {
-                            "title": "Carousel"
-                        },
-                        {
-                            "title": "Suggestions"
-                        }
-                    ]
-                }
-            },
-            "possibleIntents": [
-                {
-                    "intent": "actions.intent.OPTION",
-                    "inputValueData": {
-                        "@type": "type.googleapis.com/google.actions.v2.OptionValueSpec",
-                        "carouselSelect": {
-                            "items": [
-                                {
-                                    "optionInfo": {
-                                        "key": "MATH_AND_PRIME",
-                                        "synonyms": [
-                                            "math",
-                                            "math and prime",
-                                            "prime numbers",
-                                            "prime"
-                                        ]
-                                    },
-                                    "title": "Math & prime numbers",
-                                    "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
-                                    "image": {
-                                        "url": "http://example.com/math_and_prime.jpg",
-                                        "accessibilityText": "Math & prime numbers"
-                                    }
-                                },
-                                {
-                                    "optionInfo": {
-                                        "key": "EGYPT",
-                                        "synonyms": [
-                                            "religion",
-                                            "egpyt",
-                                            "ancient egyptian"
-                                        ]
-                                    },
-                                    "title": "Ancient Egyptian religion",
-                                    "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
-                                    "image": {
-                                        "url": "http://example.com/egypt",
-                                        "accessibilityText": "Egypt"
-                                    }
-                                },
-                                {
-                                    "optionInfo": {
-                                        "key": "RECIPES",
-                                        "synonyms": [
-                                            "recipes",
-                                            "recipe",
-                                            "42 recipes"
-                                        ]
-                                    },
-                                    "title": "42 recipes with 42 ingredients",
-                                    "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
-                                    "image": {
-                                        "url": "http://example.com/recipe",
-                                        "accessibilityText": "Recipe"
+                                            'isSsml': false,
+                                            'noInputPrompts': [],
+                                            'richResponse': {
+                                                'items': [{
+                                                        'simpleResponse': {
+                                                            'textToSpeech': "Hi",
+                                                            'displayText': "Hi"
+                                                        }
+                                                    },
+                                                    {
+                                                        'basicCard': {
+                                                            'title': 'VIKI',
+                                                            'buttons': [{
+                                                                'title': 'Login to Viki',
+                                                                'openUrlAction': {
+                                                                    'url': "https://vikii.herokuapp.com/login?id="
+                                                                }
+                                                            }]
+                                                        }
+                                                    }
+                                                ]
+                                            }
+                                        }
                                     }
                                 }
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
-    ]
-                                            
-                                        };
                             } else {
                                 speech = speech + "" + os.EOL + "" + data.title + "! ";
                                 speech = speech + "\n More @ : " + shortUrl + "!" + os.EOL;
