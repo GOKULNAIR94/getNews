3b5cb2310a6cbc882d8a683570b0c527674d4c28
Gokul Nair <gokulgnair94@gmail.com>
2017-09-23 14:23:17 +0530

    News

--- server.js
+++ server.js
@@ -87,15 +87,58 @@ restService.post('/inputmsg', function(req, res) {
                                                         }
                                                     },
                                                     {
-                                                        'basicCard': {
-                                                            'title': 'VIKI',
-                                                            'buttons': [{
-                                                                'title': 'Login to Viki',
-                                                                'openUrlAction': {
-                                                                    'url': "https://vikii.herokuapp.com/login?id="
-                                                                }
-                                                            }]
+                                                        "carouselSelect": {
+                                                    "items": [{
+                                                            "optionInfo": {
+                                                                "key": "MATH_AND_PRIME",
+                                                                "synonyms": [
+                                                                    "math",
+                                                                    "math and prime",
+                                                                    "prime numbers",
+                                                                    "prime"
+                                                                ]
+                                                            },
+                                                            "title": "Math & prime numbers",
+                                                            "description": "42 is an abundant number because the sum of its proper divisors 54 is greater…",
+                                                            "image": {
+                                                                "url": "https://image.flaticon.com/teams/slug/freepik.jpg",
+                                                                "accessibilityText": "Math & prime numbers"
+                                                            }
+                                                        },
+                                                        {
+                                                            "optionInfo": {
+                                                                "key": "EGYPT",
+                                                                "synonyms": [
+                                                                    "religion",
+                                                                    "egpyt",
+                                                                    "ancient egyptian"
+                                                                ]
+                                                            },
+                                                            "title": "Ancient Egyptian religion",
+                                                            "description": "42 gods who ruled on the fate of the dead in the afterworld. Throughout the under…",
+                                                            "image": {
+                                                                "url": "https://image.flaticon.com/teams/slug/freepik.jpg",
+                                                                "accessibilityText": "Egypt"
+                                                            }
+                                                        },
+                                                        {
+                                                            "optionInfo": {
+                                                                "key": "RECIPES",
+                                                                "synonyms": [
+                                                                    "recipes",
+                                                                    "recipe",
+                                                                    "42 recipes"
+                                                                ]
+                                                            },
+                                                            "title": "42 recipes with 42 ingredients",
+                                                            "description": "Here's a beautifully simple recipe that's full of flavor! All you need is some ginger and…",
+                                                            "image": {
+                                                                "url": "https://image.flaticon.com/teams/slug/freepik.jpg",
+                                                                "accessibilityText": "Recipe"
+                                                            }
                                                         }
+                                                    ]
+                                                }
                                                     }
                                                 ]
                                             }
