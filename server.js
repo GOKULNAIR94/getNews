1b04276ba87288f5a45b8797e67d2cd3777ec31c
Gokul Nair <gokulgnair94@gmail.com>
2017-09-26 22:45:11 +0530

    news

--- server.js
+++ server.js
@@ -113,8 +113,8 @@ restService.post('/inputmsg', function(req, res) {
                                                                     "title": "open",
                                                                     "description": data.title + ".",
                                                                     "image": {
-                                                                        "url": "",
-                                                                        "accessibilityText": "Test1"
+                                                                        "url": "https://lh3.googleusercontent.com/Nu3a6F80WfixUqf_ec_vgXy_c0-0r4VLJRXjVFF_X_CIilEu8B9fT35qyTEj_PEsKw",
+                                                                        "accessibilityText": ""
                                                                     }
                                                                 },{
                                                                     "optionInfo": {
