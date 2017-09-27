01525d3d221fcf1d1de7195d83d9fb7c531d0f9e
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-09-27 16:50:19 +0530

    G Rss

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ restService.post('/inputmsg', function(req, res) {
                         
                         console.log( "flag :" + resp[i]["title"].indexOf(headLine.substring(1, 10)) == 0 );
                         console.log( "resp[i].title :" + resp[i].title );
-                        if( resp[i]["title"].indexOf(headLine.substring(1, 10)) == 0  ){
+                        if( resp[i].title == headLine ){
                             basicCard["title"] = resp[i].title;
                             basicCard["image"] = resp[i].thumbnailUrl;
                             basicCard["description"] = resp[i].description;
